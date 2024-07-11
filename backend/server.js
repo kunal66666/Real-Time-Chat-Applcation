fd7d9351ff6bb0e1ff9bed2f9f7240f8f2b4eb56
kunal66666 <kunal.kashyap.fiem.cse19@teamfuture.in>
2024-07-11 15:53:48 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
@@ -7,13 +8,19 @@ import userRoutes from './routes/user.routes.js';
 import connectToDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
 dotenv.config()
-const PORT = process.env.PORT || 8000;
 
+const PORT = process.env.PORT || 8000;
+const __dirname=path.resolve();
 app.use(express.json()); // to parse income request  with json payload( from req.body)
 app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 // app.get("/", (req, res) => {
 //     // root route
